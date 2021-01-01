bd59fea8db0191ff7613592d2874cde238814671
Kamela Williamson <kami63116@yahoo.com>
2020-12-31 17:29:08 -0700

    flake8 isn't working it shows not found and the line is too long on one so i'll keep playing

--- morse.py
+++ morse.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 """
 Morse Code Decoder
-
 "Dot" – is 1 time unit long.
 "Dash" – is 3 time units long.
 Pause between dots and dashes in a character – is 1 time unit long.
@@ -10,14 +10,36 @@ Pause between characters inside a word – is 3 time units long.
 Pause between words – is 7 time units long.
 """
 __author__ = 'kamela williamson'
+# Got help from April
 # used code from first morse code assessment
+# rewatched Daniels' morse code demo
+# did the morse code advanced on codewars
+# https://www.geeksforgeeks.org/python-regex-re-search-vs-re-findall/
+# https://www.w3schools.com/python/python_regex.asp
+# https://www.w3schools.com/python/ref_string_strip.asp
+# https://www.w3schools.com/python/ref_string_split.asp
+# https://www.geeksforgeeks.org/morse-code-translator-python/
+# https://blog.finxter.com/a-simple-python-morse-code-translator/
+# https://www.geeksforgeeks.org/morse-code-implementation/
 
+
+import re
 from morse_dict import MORSE_2_ASCII
+# starts with 0's and 1's first. get from 0's and 1's to
+# dots and dashes. standard was dots and dashes to alphabet. not
+# receiving same input as standard. there's a pattern that maps to dots &
+# dashes. and a variable factor.  how fast operator was pushing key
+# switch matters.
 
 
 def decode_bits(bits):
     # your code here
-    pass
+    # str = ""
+    bits = bits.strip('0')
+    # find the least amount of times that 0 or 1 comes up
+    tm = min(len(mc) for mc in re.findall(r'1+|0+', bits))
+    i = bits[::tm].replace('111', '-').replace('1', '.').replace('0000000', '   ').replace('000', ' ').replace('0', '')
+    return i
 
 
 def decode_morse(morse):
